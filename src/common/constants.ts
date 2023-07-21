8cac035968ca3c74b640a53d7ef9ff0f80bb5880
Dharmam Savani <savanidharmam5@gmail.com>
2023-07-21 12:27:46 -0400

    localhost support

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,5 +3,5 @@ export const constants = {
   DEFAULT_DEBOUNCE_TIME: 0,
   DEFAULT_MAX_SIZE_IN_MB: 2048,
   DEFAULT_OPENAI_MODEL: 'text-embedding-ada-002',
-  OPENAI_API_URL: 'https://api.openai.com/v1/embeddings',
+  OPENAI_API_URL: 'localhost:8000/v1/embeddings',
 };
